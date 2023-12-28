9c8b90766e474e1ce4ffaf31f23806a01f0763d6
saudurhasibuan <saudurh@gmail.com>
2023-12-28 11:16:10 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Siswa from './SiswaView.vue'
+// import axios from 'axios'
+</script>
+
+<template>
+  <main>
+    <Siswa/>
+  </main>
+</template>
+
---
+++ src/views/SiswaView.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+    <li>{{data.response}}</li>
+    </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+//   data() {
+//     return {
+//       responseData: null,
+//       error: null
+//     };
+//   },
+  mounted() {
+    // Gantilah URL dengan endpoint API yang sesuai
+    const apiUrl = 'http://localhost:8080/semua';
+
+
+    axios.get(apiUrl)
+      .then(response => {
+        // Handle response data di sini
+        this.responseData = response.data;
+      })
+      .catch(error => {
+        // Handle error di sini
+        this.error = error;
+      });
+  }
+}
+
+
+</script>
